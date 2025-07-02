74f64a681fb14a8386d371ff885e02c04d62e127
7thResonance <viswachakravarthy@gmail.com>
2025-07-03 02:35:29 +0530

    Project Time Tracker

--- Project/7R Project Time Tracker (GUI).lua
+++ Project/7R Project Time Tracker (GUI).lua
@@ -3,9 +3,6 @@
 @author 7thResonance
 @version 1.0
 @changelog initial
-@provides
-  [main] 7R Project Time Tracker (GUI).lua
-  [main] 7R Project Time Tracker (Background).lua
 @about Simplified Project time tracker. Original script made by Archie. Add Background script to autostart
 --]]
 
